e4e9cdf89b5dd38c7c106c976e4fe2411bfc8af6
OcarinhaOfTime <vagnerseibert@gmail.com>
2017-11-10 13:09:46 -0800

    First commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public float dist2stop = 2;
+    private Rigidbody2D rb2d;
+    private Vector2 startPos;
+    private bool stoped = false;
+
+    private void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch(float force, Vector2 dir) {
+        stoped = false;
+        startPos = rb2d.position;
+        rb2d.AddForce(dir * force, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        var dist = Vector2.Distance(startPos, rb2d.position);
+        print(dist);
+        if (collision.collider.CompareTag("Ball") && dist > dist2stop && !stoped) {
+            print("stoooop");
+            //rb2d.velocity = Vector2.zero;
+        }
+
+        if(collision.collider.CompareTag("Ball"))
+            stoped = true;
+    }
+}
---
+++ Assets/Scripts/BallLaunch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLaunch : MonoBehaviour {
+    public float force = 10;
+    public Ball ball;
+
+    private Camera cam;
+
+    private void Start() {
+        cam = Camera.main;
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector2 worldMousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            var dir = ((Vector2)ball.transform.position - worldMousePos).normalized;
+
+            ball.Launch(force, dir);
+        }
+    }
+}
